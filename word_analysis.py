9d8521deae9f5598760a0c8bd1d5831084695cb6
HarukiKinoshita <harukikinoshita@outlook.com>
2022-11-22 10:05:00 +0100

    Use class

--- word_analysis.py
+++ word_analysis.py
@@ -1,10 +1,21 @@
 import sys
 import spacy
 args = sys.argv
-nlp_ca = spacy.load("ca_core_news_sm")
 
-f_ca = open(args[1], 'r', encoding='UTF-8')
-data_ca = f_ca.read()
+trained_pipelines = {
+  "ca": "ca_core_news_sm",
+  "es": "es_core_news_sm"
+}
+
+
+class LangSet:
+  def __init__(self, code, nlp, source_file):
+    self.code = code
+    self.nlp = nlp
+    self.source_file = source_file
+    self.doc = nlp(source_file.read())
+    self.word_list = []
+    self.part_of_speech_dict = {}
 
 
 class Word:
@@ -13,17 +24,32 @@ class Word:
     self.pos = pos
     self.alpha = alpha
 
-list_ca = []
-doc_ca = nlp_ca(data_ca)
 
-for token in doc_ca:
-  list_ca.append(Word(token.text, token.pos_, token.is_alpha))
+def main():
+  lang = LangSet(
+    args[1],
+    spacy.load(trained_pipelines[args[1]]),
+    open(args[2], 'r', encoding='UTF-8')
+  )
 
-dict = {}
-for n in list_ca:
-  dict.setdefault(n.pos, 0)
-  dict[n.pos] += 1
+  for token in lang.doc:
+    lang.word_list.append(Word(token.text, token.pos_, token.is_alpha))
+  for n in lang.word_list:
+    lang.part_of_speech_dict.setdefault(n.pos, 0)
+    lang.part_of_speech_dict[n.pos] += 1
 
-print(dict)
+  return lang
 
-f_ca.close()
+
+if args[1] == '--help' or args[1] == '-h':
+  print("> python3 word_analysis_class.py <lang code ('ca', 'es')> <path to input file>")
+
+else:
+  lang = main()
+
+  total = sum(lang.part_of_speech_dict.values())
+  noun = lang.part_of_speech_dict['NOUN']
+  print("lang code | # total | # of nouns | % of nouns")
+  print(lang.code, total, noun, noun/total,)
+
+  lang.source_file.close()
